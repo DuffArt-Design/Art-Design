34113235d96f6c56830d2eb241f5ef7e542e23d7
Stevenrej <steveo732@gmail.com>
2023-02-08 20:03:09 -0600

    inital scaffolding - Can retreive photos from cloudinary

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/photos')
+      .then(res => res.json())
+      .then(data => setPhotos(data.resources))
+      .catch(err => console.error(err));
+  }, []);
+
+  console.log(photos);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to my website</h1>
+        <p>This is some sample text</p>
+        <hr />
+        <h2>Photos</h2>
+        {photos.map(photo => (
+          <img key={photo.public_id} src={photo.url} alt={photo.public_id} />
+        ))}
       </header>
     </div>
   );
-}
+};
 
 export default App;
