6bd493c5d174097d83359167f99c62378cde85b1
Stevenrej <steveo732@gmail.com>
2023-02-11 16:32:41 -0600

    about styling changed to add background

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -15,7 +15,7 @@ export default function About() {
           />
         </div>
         <div className='about'>
-          <p>
+          <p className='text'>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Non quam lacus suspendisse faucibus interdum posuere. At augue eget arcu dictum varius duis at consectetur. Quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Scelerisque fermentum dui faucibus in ornare quam viverra. Purus gravida quis blandit turpis. Luctus venenatis lectus magna fringilla urna porttitor. Risus in hendrerit gravida rutrum quisque non. Et molestie ac feugiat sed lectus vestibulum mattis ullamcorper. Tortor id aliquet lectus proin nibh nisl condimentum. Gravida cum sociis natoque penatibus et magnis dis parturient montes. Eget magna fermentum iaculis eu non diam phasellus vestibulum. Ac turpis egestas integer eget aliquet nibh praesent. Nisl condimentum id venenatis a condimentum.
           </p>
         </div>
