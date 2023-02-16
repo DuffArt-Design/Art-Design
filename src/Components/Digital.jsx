3462528740c7acd4ffe7b2d10d8a92ba5b9f6250
Stevenrej <steveo732@gmail.com>
2023-02-16 16:09:55 -0600

    added deleted functionality for admin

--- src/Components/Digital.jsx
+++ src/Components/Digital.jsx
@@ -1,9 +1,7 @@
-import { Image, Modal } from '@mantine/core';
+import { Image, Indicator, Modal } from '@mantine/core';
 import React, { useState, useEffect } from 'react';
 
-
-
-export default function Digital() {
+export default function Digital({ loggedIn }) {
   const [photos, setPhotos] = useState([]);
   const [opened, setOpened] = useState(false);
   const [selectedPic, setSelectedPic] = useState('');
@@ -11,9 +9,27 @@ export default function Digital() {
   // const leftColumnRef = useRef(null);
 
   // local use http://localhost:3001/photos
-  // delpoyed use https://duff-server.onrender.com/photos
+  // deployed use https://duff-server.onrender.com/photos
 
-  useEffect(() => {
+  const handleDelete = (photo) => {
+    fetch(`https://duff-server.onrender.com/pictures/${photo._id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        fetch(`https://duff-server.onrender.com/pictures`)
+          .then(res => res.json())
+          .then(data => {
+            console.log('data:', data); // log the response from the server
+            const filteredData = data.filter(photo => photo.id.startsWith("digital"));
+            setPhotos(filteredData);
+          })
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  };
+  
+  
+  const fetchPhotos = () => {
     fetch(`https://duff-server.onrender.com/pictures`)
       .then(res => res.json())
       .then(data => {
@@ -21,12 +37,12 @@ export default function Digital() {
         setPhotos(filteredData);
       })
       .catch(err => console.error(err));
+  };
+  
+  useEffect(() => {
+    fetchPhotos();
   }, []);
   
-
-
-
-  console.log(photos);
 
 
   
@@ -40,64 +56,77 @@ export default function Digital() {
         opened={opened}
         onClose={() => setOpened(false)}
       >
-  {selectedPic && (
-    <>
-      <Image
-        height={710}
-        fit="contain"
-        src={selectedPic.url}
-        alt={selectedPic._id}
-      />
-      <h2>{selectedPic.name}</h2>
-      <p>{selectedPic.description}</p>
-      <p>{selectedPic.text}</p>
-    </>
-  )}
+        {selectedPic && (
+          <>
+            <Image
+              height={710}
+              fit="contain"
+              src={selectedPic.url}
+              alt={selectedPic._id}
+            />
+            <h2>{selectedPic.name}</h2>
+            <p>{selectedPic.description}</p>
+            <p>{selectedPic.text}</p>
+          </>
+        )}
       </Modal>
       <div className='big-container'>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 0).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 1).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 2).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
       </div>
     </>
-  )  
+  );
+
 }
--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -1,19 +1,35 @@
-import { Image, Modal } from '@mantine/core';
-import React, { useState, useEffect, useRef } from 'react';
+import { Image, Indicator, Modal } from '@mantine/core';
+import React, { useState, useEffect } from 'react';
 
-
-
-export default function Photos() {
+export default function Photos({ loggedIn }) {
   const [photos, setPhotos] = useState([]);
   const [opened, setOpened] = useState(false);
   const [selectedPic, setSelectedPic] = useState('');
 
-  const leftColumnRef = useRef(null);
+  // const leftColumnRef = useRef(null);
 
   // local use http://localhost:3001/photos
-  // delpoyed use https://duff-server.onrender.com/photos
+  // deployed use https://duff-server.onrender.com/photos
 
-  useEffect(() => {
+  const handleDelete = (photo) => {
+    fetch(`https://duff-server.onrender.com/pictures/${photo._id}`, {
+      method: 'DELETE',
+    })
+      .then(() => {
+        fetch(`https://duff-server.onrender.com/pictures`)
+          .then(res => res.json())
+          .then(data => {
+            console.log('data:', data); // log the response from the server
+            const filteredData = data.filter(photo => photo.id.startsWith("photos"));
+            setPhotos(filteredData);
+          })
+          .catch(err => console.error(err));
+      })
+      .catch(err => console.error(err));
+  };
+  
+  
+  const fetchPhotos = () => {
     fetch(`https://duff-server.onrender.com/pictures`)
       .then(res => res.json())
       .then(data => {
@@ -21,30 +37,12 @@ export default function Photos() {
         setPhotos(filteredData);
       })
       .catch(err => console.error(err));
-  }, []);
-
+  };
+  
   useEffect(() => {
-    if (leftColumnRef.current) {
-      const leftColumnItems = Array.from(leftColumnRef.current.children);
-      const rightColumnItems = leftColumnItems.splice(0, Math.ceil(leftColumnItems.length / 2));
+    fetchPhotos();
+  }, []);
   
-      const leftColumnHeights = leftColumnItems.map(item => item.offsetHeight);
-      const rightColumnHeights = rightColumnItems.map(item => item.offsetHeight);
-  
-      rightColumnHeights.forEach((height, index) => {
-        if (height > leftColumnHeights[index]) {
-          rightColumnItems[index].classList.add('adjust-height');
-        }
-      });
-  
-      rightColumnItems.forEach((item, index) => {
-        if (index % 2 === 1) {
-          item.classList.add('alternate');
-        }
-      });
-    }
-  }, [photos]);
-
 
 
   
@@ -58,64 +56,77 @@ export default function Photos() {
         opened={opened}
         onClose={() => setOpened(false)}
       >
-  {selectedPic && (
-    <>
-      <Image
-        height={710}
-        fit="contain"
-        src={selectedPic.url}
-        alt={selectedPic._id}
-      />
-      <h2>{selectedPic.name}</h2>
-      <p>{selectedPic.description}</p>
-      <p>{selectedPic.text}</p>
-    </>
-  )}
+        {selectedPic && (
+          <>
+            <Image
+              height={710}
+              fit="contain"
+              src={selectedPic.url}
+              alt={selectedPic._id}
+            />
+            <h2>{selectedPic.name}</h2>
+            <p>{selectedPic.description}</p>
+            <p>{selectedPic.text}</p>
+          </>
+        )}
       </Modal>
       <div className='big-container'>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 0).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 1).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 2).map(photo => (
-            <Image
-              onClick={() => {
-                setSelectedPic(photo);
-                setOpened(true);
-              }}
-              className={'image'}
-              key={photo._id}
-              src={photo.url}
-              alt={photo._id}
-            />
+            <div key={photo._id}>
+              <Image
+                onClick={() => {
+                  setSelectedPic(photo);
+                  setOpened(true);
+                }}
+                className={'image'}
+                src={photo.url}
+                alt={photo._id}
+              />
+              {loggedIn && (
+                <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+              )}
+            </div>
           ))}
         </div>
       </div>
     </>
-  )  
+  );
+
 }
