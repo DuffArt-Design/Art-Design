4513f4c73b58b761cb0d0895a7078bbd604e5480
Stevenrej <steveo732@gmail.com>
2023-02-27 16:10:00 -0600

    loading added when images are not present

--- src/Components/Digital.jsx
+++ src/Components/Digital.jsx
@@ -5,6 +5,7 @@ export default function Digital({ loggedIn }) {
   const [photos, setPhotos] = useState([]);
   const [opened, setOpened] = useState(false);
   const [selectedPic, setSelectedPic] = useState('');
+  const [loading, setLoading] = useState(true);
 
   const handleDelete = (photo) => {
     fetch(`${process.env.REACT_APP_SERVER_URL}/${photo._id}`, {
@@ -17,59 +18,67 @@ export default function Digital({ loggedIn }) {
             console.log('data:', data); // log the response from the server
             const filteredData = data.filter(photo => photo.id.startsWith("digital"));
             setPhotos(filteredData);
+            setLoading(false);
           })
           .catch(err => console.error(err));
       })
       .catch(err => console.error(err));
   };
-  
-  
+
+
   const fetchPhotos = () => {
     fetch(process.env.REACT_APP_SERVER_URL)
       .then(res => res.json())
       .then(data => {
         const filteredData = data.filter(photo => photo.id.startsWith("digital"));
         setPhotos(filteredData);
+        setLoading(false);
       })
       .catch(err => console.error(err));
   };
-  
+
   useEffect(() => {
     fetchPhotos();
   }, []);
-  
 
 
-  
+
+
   return (
     <>
-<Modal
-  size="85%"
-  overlayColor='white'
-  overlayOpacity={0.55}
-  overlayBlur={3}
-  opened={opened}
-  onClose={() => setOpened(false)}
->
-  {selectedPic && (
-    <>
-      <Image
-        height={710}
-        fit="contain"
-        src={selectedPic.url}
-        alt={selectedPic._id}
-      />
-      <div style={{ textAlign: 'center' }}>
-      <Text fz="lg" fw={700}>{selectedPic.name}</Text>
-      <Text fs="italic">{selectedPic.description}</Text>
-      {selectedPic.text && (
-        <Text c="dimmed">From the Artist: "{selectedPic.text}"</Text>
-      )
-      }
-      </div>
-    </>
-  )}
-</Modal>
+      {loading && <div >
+        <h1 className='loading'>Loading Images...</h1>
+        <div class="icon-container">
+          <i class="fas fa-circle"></i>
+        </div>
+      </div>}
+      <Modal
+        size="85%"
+        overlayColor='white'
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        opened={opened}
+        onClose={() => setOpened(false)}
+      >
+        {selectedPic && (
+          <>
+            <Image
+              height={710}
+              fit="contain"
+              src={selectedPic.url}
+              alt={selectedPic._id}
+            />
+            <div style={{ textAlign: 'center' }}>
+              <Text fz="lg" fw={700}>{selectedPic.name}</Text>
+              <Text fs="italic">{selectedPic.description}</Text>
+              {selectedPic.text && (
+                <Text c="dimmed">From the Artist: "{selectedPic.text}"</Text>
+              )
+              }
+            </div>
+          </>
+        )}
+      </Modal>
       <div className='big-container'>
         <div className="images-container">
           {photos.filter((item, index) => index % 3 === 0).map(photo => (
