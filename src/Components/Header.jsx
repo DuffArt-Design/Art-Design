bbb9f877d0928859f063703d61fa44cc407b7f52
Stevenrej <steveo732@gmail.com>
2023-02-27 15:53:19 -0600

    mobile responsiveness and new styling added - icons and styles changed for tabs - burger added when screen size is small

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,23 +1,44 @@
-import React from 'react';
-import { Group, Header, Tabs } from '@mantine/core';
+import React, { useState } from 'react';
+import { Burger, Drawer, Group, Header, NavLink, Tabs } from '@mantine/core';
 import { useNavigate } from 'react-router-dom';
 
+
 export default function HeaderComponent() {
+  const [opened, setOpened] = useState(false);
+  const [active, setActive] = useState('');
   const navigate = useNavigate();
-  
+  const title = opened ? 'Close navigation' : 'Open navigation';
+
+  const handleNavClick = (value) => {
+    setActive(value);
+
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    if(value === 'home') {
+      value = '';
+    }
+    navigate('/' + value);
+  }
+
   return (
     <Header height={75} fixed={false} className="header">
       <Group position="apart" className="header__group">
         <h1 className="header__group__title">Duff Art & Design</h1>
         <Group position="right" className="header__group__nav">
-          <Tabs className="header__group__nav__segment">
+          <Tabs className="header__group__nav__segment" variant="pills" defaultValue="home" radius="xs">
             <Tabs.List>
               <Tabs.Tab
-                color="indigo"
+                color="dark"
                 value="home"
+                icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+                  <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                  <path d="M10 12h4v4h-4z"></path>
+                </svg>}
                 onClick={() => {
                   window.scrollTo({ top: 0, behavior: 'smooth' });
                   navigate('/');
+                  setActive('home');
                 }}
               >
                 Home
@@ -25,19 +46,33 @@ export default function HeaderComponent() {
               <Tabs.Tab
                 color="teal"
                 value="digital"
+                icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-desktop" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M3 4m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z"></path>
+                  <path d="M7 20l10 0"></path>
+                  <path d="M9 16l0 4"></path>
+                  <path d="M15 16l0 4"></path>
+                </svg>}
                 onClick={() => {
                   window.scrollTo({ top: 0, behavior: 'smooth' });
                   navigate('/digital');
+                  setActive('digital');
                 }}
               >
                 Digital Art
               </Tabs.Tab>
               <Tabs.Tab
-                color="lime"
+                color="indigo"
                 value="pen_ink"
+                icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+                  <path d="M13.5 6.5l4 4"></path>
+                </svg>}
                 onClick={() => {
                   window.scrollTo({ top: 0, behavior: 'smooth' });
                   navigate('/pen_ink');
+                  setActive('pen_ink');
                 }}
               >
                 Pen & Ink
@@ -45,9 +80,15 @@ export default function HeaderComponent() {
               <Tabs.Tab
                 color="orange"
                 value="photos"
+                icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2"></path>
+                  <path d="M12 13m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                </svg>}
                 onClick={() => {
                   window.scrollTo({ top: 0, behavior: 'smooth' });
                   navigate('/photos');
+                  setActive('photos');
                 }}
               >
                 Photos
@@ -55,15 +96,111 @@ export default function HeaderComponent() {
               <Tabs.Tab
                 color="pink"
                 value="about"
+                icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                  <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                  <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+                </svg>}
                 onClick={() => {
                   window.scrollTo({ top: 0, behavior: 'smooth' });
                   navigate('/about');
+                  setActive('about');
                 }}
               >
                 About
               </Tabs.Tab>
             </Tabs.List>
           </Tabs>
+          <Burger
+            opened={opened}
+            onClick={() => setOpened((o) => !o)}
+            title={title}
+            color="#FFFFFF"
+            size="lg"
+            className="header__group__nav__burger"
+          />
+          <Drawer
+            opened={opened}
+            onClose={() => setOpened(false)}
+            title="Duff Art & Design"
+            padding="xl"
+            size="sm"
+            position="right"
+            overlayOpacity={0.55}
+            overlayBlur={3}
+          >
+            <NavLink
+              color="dark"
+              label='Home'
+              active={'home' === active}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-home-2" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+                <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+                <path d="M10 12h4v4h-4z"></path>
+              </svg>}
+              onClick={() => {
+                handleNavClick('home')
+              }}
+            />
+            <NavLink
+              color="teal"
+              label="Digital Art"
+              active={'digital' === active}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-device-desktop" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 4m0 1a1 1 0 0 1 1 -1h16a1 1 0 0 1 1 1v10a1 1 0 0 1 -1 1h-16a1 1 0 0 1 -1 -1z"></path>
+                <path d="M7 20l10 0"></path>
+                <path d="M9 16l0 4"></path>
+                <path d="M15 16l0 4"></path>
+              </svg>}
+              onClick={() => {
+                handleNavClick('digital')
+              }}
+            />
+            <NavLink
+              color="indigo"
+              label="Pen & Ink"
+              active={'pen_ink' === active}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-pencil" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M4 20h4l10.5 -10.5a1.5 1.5 0 0 0 -4 -4l-10.5 10.5v4"></path>
+                <path d="M13.5 6.5l4 4"></path>
+              </svg>}
+              onClick={() => {
+                handleNavClick('pen_ink')
+              }}
+            />
+            <NavLink
+              color="orange"
+              label="Photos"
+              active={'photos' === active}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-camera" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M5 7h1a2 2 0 0 0 2 -2a1 1 0 0 1 1 -1h6a1 1 0 0 1 1 1a2 2 0 0 0 2 2h1a2 2 0 0 1 2 2v9a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2v-9a2 2 0 0 1 2 -2"></path>
+                <path d="M12 13m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+              </svg>}
+              onClick={() => {
+                handleNavClick('photos')
+              }}
+            />
+            <NavLink
+              color="pink"
+              label="About"
+              active={'about' === active}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="20" height="20" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0"></path>
+                <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0"></path>
+                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855"></path>
+              </svg>}
+              onClick={() => {
+                handleNavClick('about')
+              }}
+            />
+          </Drawer>
+
         </Group>
       </Group>
     </Header>
