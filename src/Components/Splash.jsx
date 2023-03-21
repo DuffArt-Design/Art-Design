ecf93821f0c64dcaacde0ca7963611a08586e010
Stevenrej <steveo732@gmail.com>
2023-03-20 20:04:45 -0500

    use effect added to splash component

--- src/Components/Splash.jsx
+++ src/Components/Splash.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Image } from '@mantine/core';
 
 export default function Splash() {
   const isMobile = window.matchMedia('(max-width: 924px)').matches;
 
-  
+  useEffect(() => {
+    const spinUpBackend = async () => {
+      await fetch(process.env.REACT_APP_SPIN);
+    };
+    spinUpBackend();
+  }, []);
+
 //   first src is for mobile homepage image
 //   second src is for desktop homepage image
 
