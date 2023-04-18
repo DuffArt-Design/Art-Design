05c44361bca1c732308d3d679aba86a6662ae689
Stevenrej <steveo732@gmail.com>
2023-04-18 15:13:32 -0500

    format updated

--- src/Components/Photos.jsx
+++ src/Components/Photos.jsx
@@ -27,124 +27,124 @@ export default function Photos({ loggedIn, photos, setPhotos, loading, error }) 
 
   return (
     <>
-        {error && (
-          <p>Error Loading Images</p>
-        )}
-        {loading && (
-          <motion.div
-            key="photos-loading"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 0.25 } }}
-            exit={{ opacity: 0, transition: { duration: 0.25 } }}
-          >
-            <div className={`loading_container`}>
-              <Group position="center">
-                <h1 className='loading'>Loading Images</h1>
-                <div class={`icon-container`}>
-                  <BarLoader
-                    color='white'
-                    height={10}
-                    width={1500}
-                  />
-                </div>
-              </Group>
-            </div>
-          </motion.div>
-        )}
-        <Modal
-          size="85%"
-          overlayColor='black'
-          overlayOpacity={0.55}
-          overlayBlur={3}
-          opened={opened}
-          onClose={() => setOpened(false)}
-        >
-
-          {selectedPic && (
-            <>
-              <Image
-                height={710}
-                fit="contain"
-                src={selectedPic.url}
-                alt={selectedPic._id}
-
-              />
-              <div style={{ textAlign: 'center' }}>
-                <Text c="white" fz="lg" fw={700}>{selectedPic.name}</Text>
-                <Text c="white" fs="italic">{selectedPic.description}</Text>
-                {selectedPic.text && (
-                  <Text c="dimmed">From the Artist: "{selectedPic.text}"</Text>
-                )
-                }
-              </div>
-            </>
-          )}
-        </Modal>
-        {!loading && (
+      {error && (
+        <p>Error Loading Images</p>
+      )}
+      {loading && (
         <motion.div
-        key="photo-motion"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 0.75 } }}
-        exit={{ opacity: 0, transition: { duration: 0.75 } }}
+          key="photos-loading"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.25 } }}
+          exit={{ opacity: 0, transition: { duration: 0.25 } }}
+        >
+          <div className={`loading_container`}>
+            <Group position="center">
+              <h1 className='loading'>Loading Images</h1>
+              <div class={`icon-container`}>
+                <BarLoader
+                  color='white'
+                  height={10}
+                  width={1500}
+                />
+              </div>
+            </Group>
+          </div>
+        </motion.div>
+      )}
+      <Modal
+        size="85%"
+        overlayColor='black'
+        overlayOpacity={0.55}
+        overlayBlur={3}
+        opened={opened}
+        onClose={() => setOpened(false)}
       >
-            <div className={`big-container`}>
-              <div className="images-container">
-                {filteredData.filter((item, index) => index % 3 === 0).map(photo => (
-                  <div key={photo._id}>
-                    <Image
-                      onClick={() => {
-                        setSelectedPic(photo);
-                        setOpened(true);
-                      }}
-                      className='image'
-                      src={photo.url}
-                      alt={photo._id}
-                    />
-                    {loggedIn && (
-                      <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
-                    )}
-                  </div>
-                ))}
-              </div>
-              <div className="images-container">
-                {filteredData.filter((item, index) => index % 3 === 1).map(photo => (
-                  <div key={photo._id}>
-                    <Image
-                      onClick={() => {
-                        setSelectedPic(photo);
-                        setOpened(true);
-                      }}
-                      className='image'
-                      src={photo.url}
-                      alt={photo._id}
-                    />
-                    {loggedIn && (
-                      <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
-                    )}
-                  </div>
-                ))}
-              </div>
-              <div className="images-container">
-                {filteredData.filter((item, index) => index % 3 === 2).map(photo => (
-                  <div key={photo._id}>
-                    <Image
-                      onClick={() => {
-                        setSelectedPic(photo);
-                        setOpened(true);
-                      }}
-                      className='image'
-                      src={photo.url}
-                      alt={photo._id}
-                    />
-                    {loggedIn && (
-                      <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
-                    )}
-                  </div>
-                ))}
-              </div>
+
+        {selectedPic && (
+          <>
+            <Image
+              height={710}
+              fit="contain"
+              src={selectedPic.url}
+              alt={selectedPic._id}
+
+            />
+            <div style={{ textAlign: 'center' }}>
+              <Text c="white" fz="lg" fw={700}>{selectedPic.name}</Text>
+              <Text c="white" fs="italic">{selectedPic.description}</Text>
+              {selectedPic.text && (
+                <Text c="dimmed">From the Artist: "{selectedPic.text}"</Text>
+              )
+              }
             </div>
-          </motion.div>
+          </>
         )}
+      </Modal>
+      {!loading && (
+        <motion.div
+          key="photo-motion"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { duration: 0.75 } }}
+          exit={{ opacity: 0, transition: { duration: 0.75 } }}
+        >
+          <div className={`big-container`}>
+            <div className="images-container">
+              {filteredData.filter((item, index) => index % 3 === 0).map(photo => (
+                <div key={photo._id}>
+                  <Image
+                    onClick={() => {
+                      setSelectedPic(photo);
+                      setOpened(true);
+                    }}
+                    className='image'
+                    src={photo.url}
+                    alt={photo._id}
+                  />
+                  {loggedIn && (
+                    <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="images-container">
+              {filteredData.filter((item, index) => index % 3 === 1).map(photo => (
+                <div key={photo._id}>
+                  <Image
+                    onClick={() => {
+                      setSelectedPic(photo);
+                      setOpened(true);
+                    }}
+                    className='image'
+                    src={photo.url}
+                    alt={photo._id}
+                  />
+                  {loggedIn && (
+                    <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+                  )}
+                </div>
+              ))}
+            </div>
+            <div className="images-container">
+              {filteredData.filter((item, index) => index % 3 === 2).map(photo => (
+                <div key={photo._id}>
+                  <Image
+                    onClick={() => {
+                      setSelectedPic(photo);
+                      setOpened(true);
+                    }}
+                    className='image'
+                    src={photo.url}
+                    alt={photo._id}
+                  />
+                  {loggedIn && (
+                    <Indicator color="red" label="X" size={25} onClick={() => handleDelete(photo)}></Indicator>
+                  )}
+                </div>
+              ))}
+            </div>
+          </div>
+        </motion.div>
+      )}
     </>
   );
 }
