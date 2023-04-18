3c6560ca729032c056f8dda7910759fd53c581cf
Stevenrej <steveo732@gmail.com>
2023-04-17 19:30:31 -0500

    useLocation used to interact with Framer Motion

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+
 
 import Splash from './Splash';
 import PenInk from './PenInk';
@@ -8,33 +9,37 @@ import Digital from './Digital';
 import About from './About';
 import Upload from './Upload';
 import Secret from './Secret';
+import { AnimatePresence } from 'framer-motion';
 
 export default function Main() {
   const [loggedIn, setLoggedIn] = useState(false);
+  const location = useLocation();
 
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Splash />} />
-        <Route path="/digital" element={<Digital loggedIn={loggedIn} />} />
-        <Route path="/pen_ink" element={<PenInk loggedIn={loggedIn} />} />
-        <Route path="/photos" element={<Photos loggedIn={loggedIn} />} />
-        <Route path="/about" element={<About />} />
-        <Route
-          path="/secret"
-          element={<Secret onLogin={(loggedIn) => setLoggedIn(loggedIn)} />}
-        />
-        <Route
-          path="/upload"
-          element={
-            loggedIn ? (
-              <Upload />
-            ) : (
-              <Navigate to="/secret" />
-            )
-          }
-        />
-      </Routes>
+      <AnimatePresence mode="wait">
+      <Routes key={location.key} location={location}>
+          <Route path="/" element={<Splash />} />
+          <Route path="/digital" element={<Digital loggedIn={loggedIn} />} />
+          <Route path="/pen_ink" element={<PenInk loggedIn={loggedIn} />} />
+          <Route path="/photos" element={<Photos loggedIn={loggedIn} />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/secret"
+            element={<Secret onLogin={(loggedIn) => setLoggedIn(loggedIn)} />}
+          />
+          <Route
+            path="/upload"
+            element={
+              loggedIn ? (
+                <Upload />
+              ) : (
+                <Navigate to="/secret" />
+              )
+            }
+          />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
